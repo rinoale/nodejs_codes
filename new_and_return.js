5991a15380bba539c2cc01f6eb013111da23a961
rinoale <rinoale@hanmail.net>
2016-08-25 02:44:12 +0000

    function returns function. scope

---
+++ new_and_return.js
@@ -0,0 +1,49 @@
+function testFunc(first, second) {
+  this.first = first;
+
+  return second;
+}
+
+var tst1 = testFunc('without_new', 1);
+
+var tst2 = new testFunc('with_new', 2);
+
+console.log(tst1.constructor.name);
+
+console.log(tst2.constructor.name);
+
+console.log(tst1);
+
+console.log(tst2);
+
+
+
+
+
+function testFunction(first, second) {
+  this.first = first;
+  console.log(this);
+
+  function insideFunction(second) {
+    this.second = second;
+  }
+  return new insideFunction(second);
+}
+
+testFunction.prototype.testProto = function () {
+  console.log(this.first);
+}
+
+console.log('====== creating test1 ======');
+var test1 = testFunction('without_new', 1);
+
+console.log('====== creating test2 ======');
+var test2 = new testFunction('with_new', 2);
+
+console.log(test1.constructor.name);
+
+console.log(test2.constructor.name);
+
+console.log(test1);
+
+console.log(test2);
